a807ed8dcf51976a88c4bc882241065e9a1ed910
KimMinSung235 <alexkim0638@gmail.com>
2025-06-01 21:05:02 +0900

    shoot gun
    ..

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GunController : MonoBehaviour
+{
+    [SerializeField] private InputActionAsset inputActions;
+    [SerializeField] private RailGun gun;
+    private InputAction gunAction; 
+    void Awake()
+    {
+        if(inputActions != null)
+        {
+            gunAction = inputActions.FindActionMap("XRI Right Interaction").FindAction("Select"); // get the right-hand select action
+        }
+
+    }
+    void Update()
+    {
+        if (gunAction.WasPressedThisFrame())
+        {
+            gun.Shoot();
+        }
+    }
+}
